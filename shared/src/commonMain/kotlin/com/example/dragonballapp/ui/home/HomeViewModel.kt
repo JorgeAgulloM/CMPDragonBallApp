7b801e012468584bd545ebc956ccdb15649bb293
JorgeAgulloM <agullojorge@gmail.com>
2024-11-11 22:53:04 +0100

    Implemented Repository in ViewModel.

--- shared/src/commonMain/kotlin/com/example/dragonballapp/ui/home/HomeViewModel.kt
+++ shared/src/commonMain/kotlin/com/example/dragonballapp/ui/home/HomeViewModel.kt
@@ -1,12 +1,33 @@
 package com.example.dragonballapp.ui.home
 
+import com.example.dragonballapp.domain.Repository
+import com.example.dragonballapp.domain.model.CharacterModel
 import com.rickclephas.kmp.nativecoroutines.NativeCoroutinesState
 import com.rickclephas.kmp.observableviewmodel.MutableStateFlow
 import com.rickclephas.kmp.observableviewmodel.ViewModel
+import com.rickclephas.kmp.observableviewmodel.launch
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.withContext
 
-class HomeViewModel: ViewModel() {
+class HomeViewModel(private val repository: Repository): ViewModel() {
     private val _example = MutableStateFlow( viewModelScope,"Yorch")
     @NativeCoroutinesState
     val example: StateFlow<String> = _example
+
+    private val _character = MutableStateFlow<List<CharacterModel>>( viewModelScope, emptyList())
+    @NativeCoroutinesState
+    val character: StateFlow<List<CharacterModel>> = _character
+
+    init {
+        viewModelScope.launch {
+            val response = withContext(Dispatchers.IO) {
+                repository.getCharacters()
+            }
+            _character.update { response }
+        }
+    }
+
 }
