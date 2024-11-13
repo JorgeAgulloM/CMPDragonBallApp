957f02aebc893d756645fce015e3464593e6a15c
Jorge1 <agullojorge@gmail.com>
2024-11-13 20:36:52 +0100

    HomeScreen Android developed.

---
+++ composeApp/src/androidMain/kotlin/com/example/dragonballapp/resources/Colors.kt
@@ -0,0 +1,8 @@
+package com.example.dragonballapp.resources
+
+import androidx.compose.ui.graphics.Color
+
+//val TitleTextColor = Color(0xFFE8D0C0)
+val Orange = Color(0xFFFC6D07)
+val White = Color(0xFFF3F3F3)
+val BackGroundPrimary = Color(0xFF114B74)
--- composeApp/src/androidMain/kotlin/com/example/dragonballapp/ui/home/HomeScreen.kt
+++ composeApp/src/androidMain/kotlin/com/example/dragonballapp/ui/home/HomeScreen.kt
@@ -1,21 +1,186 @@
 package com.example.dragonballapp.ui.home
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil.compose.AsyncImage
+import com.example.dragonballapp.R
+import com.example.dragonballapp.domain.model.CharacterModel
+import com.example.dragonballapp.resources.BackGroundPrimary
+import com.example.dragonballapp.resources.Orange
+import com.example.dragonballapp.resources.White
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier) {
+fun HomeScreen(modifier: Modifier = Modifier, navigateToDetail: (Int) -> Unit) {
     val viewModel: HomeViewModel = koinViewModel()
-    val example by viewModel.example.collectAsStateWithLifecycle()
+    val characters by viewModel.characters.collectAsStateWithLifecycle()
 
-    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = example)
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(BackGroundPrimary),//.copy(alpha = 0.8f)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Goku Edition",
+            modifier.padding(4.dp),
+            fontWeight = FontWeight.Bold,
+            fontSize = 26.sp,
+            color = White
+        )
+        LazyColumn(contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)) {
+            items(characters) { character ->
+                CharacterItem(character = character) { id -> navigateToDetail(id) }
+            }
+        }
     }
+}
+
+@Composable
+fun CharacterItem(character: CharacterModel, onItemClicked: (Int) -> Unit) {
+    Box(
+        modifier = Modifier
+            .padding(16.dp)
+            .height(250.dp)
+            .clickable { onItemClicked(character.id) },
+        contentAlignment = Alignment.TopCenter
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Spacer(modifier = Modifier.weight(1f))
+            Card(
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(20),
+                border = BorderStroke(2.dp, Orange),
+                backgroundColor = White,
+                elevation = 4.dp
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = character.name, fontWeight = FontWeight.Bold, fontSize = 22.sp)
+                    Text(text = character.race, fontSize = 20.sp, fontStyle = FontStyle.Italic)
+                }
+            }
+        }
+        DragonBallShape()
+        AsyncImage(
+            model = character.image,
+            contentDescription = "Image of ${character.name}",
+            modifier = Modifier
+                .size(190.dp)
+                .padding(top = 16.dp)
+                .offset((3).dp, (2).dp),
+            colorFilter = ColorFilter.tint(Color.Black.copy(alpha = 0.6f))
+        )
+        AsyncImage(
+            model = character.image,
+            contentDescription = "Image of ${character.name}",
+            modifier = Modifier
+                .size(190.dp)
+                .padding(top = 16.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun DragonBallShape(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .size(180.dp)
+            .clip(CircleShape)
+            .background(
+                Brush.linearGradient(
+                    listOf(
+                        Orange,
+                        Color.White.copy(alpha = 0.9f),
+                        Orange.copy(alpha = 0.95f),
+                        Orange.copy(alpha = 0.9f),
+                        Orange,
+                        Orange,
+                        Orange
+                    )
+                )
+            )
+            .border(
+                BorderStroke(
+                    3.dp,
+                    Brush.linearGradient(
+                        listOf(
+                            Orange.copy(alpha = 0.8f),
+                            Color.White.copy(alpha = 0.8f),
+                            Color.White.copy(alpha = 0.7f),
+                            Orange,
+                            Orange,
+                            Orange,
+                            Orange
+                        )
+                    )
+                ),
+                CircleShape
+            )
+            .rotate(80f),
+        contentAlignment = Alignment.Center
+    ) {
+        Row {
+            Column {
+                DragonBalStart()
+                DragonBalStart()
+            }
+            Column {
+                DragonBalStart()
+                DragonBalStart()
+            }
+        }
+    }
+}
+
+@Composable
+fun DragonBalStart() {
+    Icon(
+        modifier = Modifier
+            .padding(16.dp)
+            .size(24.dp)
+            .rotate(-60f),
+        painter = painterResource(id = R.drawable.ic_start),
+        contentDescription = null,
+        tint = Color.Red
+    )
 }
