8d279682af62b7ec683e33b7b2e9fbf5fa812442
Jorge1 <agullojorge@gmail.com>
2024-11-14 17:32:07 +0100

    Created CharacterImage

---
+++ composeApp/src/androidMain/kotlin/com/example/dragonballapp/ui/componentes/CharacterImage.kt
@@ -0,0 +1,33 @@
+package com.example.dragonballapp.ui.componentes
+
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+
+@Composable
+fun CharacterImage(
+    modifier: Modifier = Modifier.size(190.dp),
+    characterImage: String,
+    characterName: String
+) {
+    AsyncImage(
+        model = characterImage,
+        contentDescription = null,
+        modifier = modifier
+            .padding(top = 16.dp)
+            .offset((6).dp, (4).dp),
+        colorFilter = ColorFilter.tint(Color.Black.copy(alpha = 0.6f))
+    )
+    AsyncImage(
+        model = characterImage,
+        contentDescription = characterName,
+        modifier = modifier
+            .padding(top = 16.dp)
+    )
+}
